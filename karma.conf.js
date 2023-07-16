fc0bb1a260654dd4a2cf15d3028c8473b4559b40
skillor <skillor@gmx.net>
2023-07-16 22:29:25 +0200

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/db-fakinator'),
+      dir: require('path').join(__dirname, './coverage/db-dummy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
